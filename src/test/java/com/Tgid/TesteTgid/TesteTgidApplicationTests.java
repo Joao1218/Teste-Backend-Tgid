32bd997bab832b0bb4fc681a797c99ccd1a79df2
Joao1218 <joaohmreinaldo@gmail.com>
2025-06-06 17:37:41 -0300

    Primeiro commit

---
+++ src/test/java/com/Tgid/TesteTgid/TesteTgidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tgid.TesteTgid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTgidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
